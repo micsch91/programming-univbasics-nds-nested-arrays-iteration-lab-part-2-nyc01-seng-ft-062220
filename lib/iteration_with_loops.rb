934e4ea0bcec1194c5a77c3474156b2e0cf55c99
Michael Schneider <mic.sch91@gmail.com>
2020-06-03 16:13:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.count do
   element_index = 0
   lowest_temp =""
 while element_index < src[row_index].count do
-  if src[row_index][element_index] > lowest_temp
+  if src[row_index][element_index] < lowest_temp
     lowest_temp = src[row_index][element_index]
 end
 element_index += 1
